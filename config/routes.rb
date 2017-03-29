bf2ce3e6645023252e770e326e2dfecc6e565f80
webappman <web730.man@outlook.com>
2017-03-29 14:27:10 +0200

    Add ChatRooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+
+  get 'chat_rooms/new'
+
+  get 'chat_rooms/create'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
